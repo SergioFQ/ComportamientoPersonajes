bfc91456439dbb55a18fdbda5af7c167b5c3c6d7
SergioFQ <sergio.fq25@gmail.com>
2021-01-02 12:28:20 +0100

    Finite State Machine in base script

--- Assets/Scripts/BaseAgent.cs
+++ Assets/Scripts/BaseAgent.cs
@@ -8,6 +8,18 @@ public class BaseAgent : MonoBehaviour
     public float maxPerceptionL;
     public int maxPerceptionA;
 
+    protected enum state
+    {
+        Seek,
+        Wander,
+        Pursuit,
+        Evade,
+        Avoid,
+        Eat,
+        Dead
+    }
+    state currentState;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,8 +36,13 @@ public class BaseAgent : MonoBehaviour
 
     private void FixedUpdate()
     {
+        Vision();
+    }
+
+    protected void Vision()
+    {
         hits.Clear();
-        for(int i = - maxPerceptionA ; i < maxPerceptionA; i += 4)
+        for (int i = -maxPerceptionA; i < maxPerceptionA; i += 4)
         {
             Vector2 v = Quaternion.Euler(0, 0, i) * transform.up;
             RaycastHit2D hit = Physics2D.Raycast(transform.position, v, maxPerceptionL);
@@ -33,19 +50,111 @@ public class BaseAgent : MonoBehaviour
             hits.Add(hit);
         }
 
-        foreach(RaycastHit2D h in hits)
+        foreach (RaycastHit2D h in hits)
         {
-            if(h.collider != null)
+            if (h.collider != null)
             {
                 CollisionDetected(h.collider);
             }
         }
-
     }
 
     // Gestionado por cada agente
     protected void CollisionDetected (Collider2D collider)
     {
 
+    }
+    protected void ChangeState(state nextState) {
+        currentState = nextState;
+    }
+    protected virtual void SeekAction()
+    {
+
+    }
+
+    protected virtual void WanderAction()
+    {
+
+    }
+    protected virtual void PursuitAction()
+    {
+
+    }
+    protected virtual void EvadeAction()
+    {
+
+    }
+    protected virtual void AvoidAction()
+    {
+
+    }
+    protected virtual void EatAction()
+    {
+
+    }
+    protected virtual void DeadAction()
+    {
+
+    }
+    //coroutines
+    IEnumerator Seek()
+    {
+        while (currentState.Equals(state.Seek))
+        {
+            SeekAction();
+            yield return 0;//retornamos algo cualquiera
+        }
+    }
+
+    IEnumerator Wander()
+    {
+        while (currentState.Equals(state.Wander))
+        {
+            WanderAction();
+            yield return 0;//retornamos algo cualquiera
+        }
+    }
+
+    IEnumerator Pursuit()
+    {
+        while (currentState.Equals(state.Pursuit))
+        {
+            PursuitAction();
+            yield return 0;//retornamos algo cualquiera
+        }
+    }
+
+    IEnumerator Evade()
+    {
+        while (currentState.Equals(state.Evade))
+        {
+            EvadeAction();
+            yield return 0;//retornamos algo cualquiera
+        }
+    }
+
+    IEnumerator Avoid()
+    {
+        while (currentState.Equals(state.Avoid))
+        {
+            AvoidAction();
+            yield return 0;//retornamos algo cualquiera
+        }
+    }
+    IEnumerator Eat()
+    {
+        while (currentState.Equals(state.Eat))
+        {
+            EatAction();
+            yield return 0;//retornamos algo cualquiera
+        }
+    }
+    IEnumerator Dead()
+    {
+        while (currentState.Equals(state.Dead))
+        {
+            DeadAction();
+            yield return 0;//retornamos algo cualquiera
+        }
     }
 }
