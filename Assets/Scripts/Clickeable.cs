3f1c0f82a67623ac063a12b70d3e5165f86159fd
plpgamedev <plpgamedev@gmail.com>
2021-01-04 14:52:56 +0100

    Minicamera that follows a target

---
+++ Assets/Scripts/Clickeable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clickeable : MonoBehaviour
+{
+    public MiniCameraCotroller miniCameraController;
+    public SpriteRenderer sR;
+    private Color mainC;
+
+    public void Awake()
+    {
+        miniCameraController = FindObjectOfType<MiniCameraCotroller>();
+        mainC = sR.color;
+    }
+
+    public void IsTarget() 
+    {
+        sR.color = Color.red;
+    }
+
+    public void IsNotTarget() 
+    {
+        sR.color = mainC;
+    }
+
+    private void OnMouseDown()
+    {
+        miniCameraController.SelectTarget(gameObject);
+    }
+}
---
+++ Assets/Scripts/MiniCameraCotroller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniCameraCotroller : MonoBehaviour
+{
+    public GameObject target;
+    public Camera cam;
+
+    public void SelectTarget(GameObject _target) 
+    {
+        if (!cam.gameObject.activeSelf) cam.gameObject.SetActive(true);
+        if (target != null) TargetToNull();
+        target = _target;
+        target.GetComponent<Clickeable>().IsTarget();
+    }
+
+    public void TargetToNull() 
+    {
+        if (target == null) return;
+        target.GetComponent<Clickeable>().IsNotTarget();
+        target = null;
+    }
+
+    public void Update()
+    {
+        if(target!=null)
+            cam.transform.position = new Vector3(target.transform.position.x, target.transform.position.y, cam.transform.position.z);
+    }
+
+}
